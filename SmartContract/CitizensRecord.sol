638bef6384e235f4765d6bb22d82c63d29e178ab
Sergio <sergio_1582@yahoo.es>
2018-10-10 16:39:07 +0200

    First contract working, uploading citizens, and consulting the information. Without validation of the caller

--- SmartContract/CitizensRecord.sol
+++ SmartContract/CitizensRecord.sol
@@ -2,19 +2,25 @@ pragma solidity^0.4.25;
 
 contract CitizensRecord {
 
-    struct citizen {
+    struct Citizen {
         string name;
-        string ID;
-        string address;
+        uint32 ID;
+        string personalAddress;
     }
 
-    Citizen[] public citizens;
     address president;
 
     constructor() public {
         president = msg.sender;
     }
-
+    
+    // This contract only defines a modifier but does not use
+    // it: it will be used in derived contracts.
+    // The function body is inserted where the special symbol
+    // `_;` in the definition of a modifier appears.
+    // This means that if the president calls this function, the
+    // function is executed and otherwise, an exception is
+    // thrown.
     modifier onlypresident() {
         require(
             msg.sender == president,
@@ -23,13 +29,39 @@ contract CitizensRecord {
         _;
     } 
 
+    // Citizen[] public citizensStorage;
+    mapping(uint32=>Citizen) citizensStorage;
+    
+    function is_available(uint32 _ID) private view returns (bool){
+                    return citizensStorage[_ID].ID == 0 ? true : false;
+    }
 
-
-    function registerCitizen(
+    function register_citizen(
         string _name, 
-        string _ID, 
-        uint _weight, 
-        uint _height) public onlydoctor {
-            citizens.push(Citizen(_name, _ID, _weight, _height));
-        }
+        uint32 _ID,
+        string _personalAddress) public onlypresident {
+           // citizensStorage.push(Citizen(_name, _ID, _adresss));
+            require(is_available( _ID ), "The ID it's not available");
+                Citizen memory citizen;
+                citizen.ID = _ID;
+                citizen.name = _name;
+                citizen.personalAddress = _personalAddress;
+                citizensStorage[_ID] = citizen;           
+    }
+
+    // function set_personalAddress(
+    //     uint32 _ID,
+    //     string _personalAddress) public onlypresident {
+        
+    //         require(!is_available( _ID ), "The ID doesn't exists");
+    //             Citizen memory citizen;
+    //             citizen = citizensStorage[_ID];
+    //             citizen.personalAddress = _personalAddress;
+    // }
+
+    function query_id(uint32 _ID) public view returns (uint32, string, string ) {
+        //require(is_available( _ID ), "The ID it's not available");
+        return (citizensStorage[_ID].ID, citizensStorage[_ID].name, citizensStorage[_ID].personalAddress);
+    }
 }
+
